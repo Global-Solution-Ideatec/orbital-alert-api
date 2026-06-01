f16e2c256db306bdf3f67b2d5b7b2a4249509c7b
cadupacheco <caducoelho.pacheco1303@gmail.com>
2026-06-01 20:57:50 -0300

    Add Alerts CRUD

---
+++ Controllers/AlertsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrbitalAlert.API.Data;
+using OrbitalAlert.API.Models;
+
+namespace OrbitalAlert.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlertsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AlertsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Alert>>> GetAlerts()
+        {
+            return await _context.Alerts
+                .Include(a => a.City)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Alert>> GetAlert(int id)
+        {
+            var alert = await _context.Alerts
+                .Include(a => a.City)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (alert == null)
+                return NotFound();
+
+            return alert;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Alert>> PostAlert(Alert alert)
+        {
+            alert.CreatedAt = DateTime.UtcNow;
+
+            _context.Alerts.Add(alert);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAlert),
+                new { id = alert.Id },
+                alert);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAlert(int id, Alert alert)
+        {
+            if (id != alert.Id)
+                return BadRequest();
+
+            _context.Entry(alert).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Alerts.Any(a => a.Id == id))
+                    return NotFound();
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAlert(int id)
+        {
+            var alert = await _context.Alerts.FindAsync(id);
+
+            if (alert == null)
+                return NotFound();
+
+            _context.Alerts.Remove(alert);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
